9b2d487157cb642c252706828495d628a61ccf8d
inv-najil <najil.arfak@innovaturelabs.com>
2025-07-29 18:12:47 +0530

    feat:admin registeration of teacher and students

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RegisterUserController;
 
-Route::post("/api/login", [AuthController::class, 'login']);
+Route::middleware(['auth:api', 'admin'])->post('/register', [RegisterUserController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
